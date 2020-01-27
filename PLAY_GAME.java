f2bf6452152a61c8f9363d3e2b411bc432eb5396
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-01-27 13:43:52 +0100

    Add insert incorrect Word to list incorrect word

--- PLAY_GAME.java
+++ PLAY_GAME.java
@@ -122,6 +122,7 @@ public class PLAY_GAME
             {
                 lifeCount -= 2;
                 indexOfDraw++;
+                notInWord.add(letterOrWord);
             }
             else
             {
